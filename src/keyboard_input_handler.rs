82f892fdd395938eb3341d5776c37ab5d7b2f064
divingseagull <chae02hb@gmail.com>
2024-08-21 14:43:31 +0900

    KeyboardInputHandler 구조체 구현. 입력된 키 반환하는 함수 구현됨.

--- src/keyboard_input_handler.rs
+++ src/keyboard_input_handler.rs
@@ -1,3 +1,26 @@
+use ratatui::crossterm::event;
+use ratatui::crossterm::event::{read, Event, KeyEvent};
+use std::time::Duration;
+
 struct KeyboardInputHandler {
-    key_buffer: [u8;16]  // TODO: change type
+    poll_rate: u64
+}
+
+
+impl KeyboardInputHandler {
+    fn event_available(&self) -> bool{
+        event::poll(Duration::from_millis(self.poll_rate)).unwrap()
+    }
+
+    pub fn get_key(&self) -> Option<KeyEvent> {
+        if self.event_available() {
+            if let Ok(Event::Key(key)) = read() {
+                Option::from(key)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
 }
